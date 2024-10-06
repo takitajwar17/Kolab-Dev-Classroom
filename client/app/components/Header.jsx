e1cacb3e195279318c98fb76ed3ef3331eda50c7
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-10-07 01:14:21 +0600

    ui updated (added logo and buttons styling)

--- client/app/components/Header.jsx
+++ client/app/components/Header.jsx
@@ -1,42 +1,45 @@
-import Link from 'next/link';
-import { UserButton, auth } from '@clerk/nextjs';
+import Image from "next/image";
+import Link from "next/link";
+import { UserButton, auth } from "@clerk/nextjs";
+
+// Import the logo image
+import KolabLogo from "../../public/kolab-logo-white.png";
 
 const Header = async ({ username }) => {
   const { userId } = auth();
 
   return (
-    <nav className='flex items-center justify-between px-8 py-6 mb-5 bg-blue-700'>
-      <div className='flex items-center'>
-        <Link href='/'>
-          <div className='text-lg font-bold text-white italic'>
-            Sprio Dev Classroom
-          </div>
+    <nav className="flex items-center justify-between px-8 py-4 bg-black">
+      {/* Logo Section */}
+      <div className="flex items-center">
+        <Link href="/">
+          <Image src={KolabLogo} alt="Kolab Logo" width={80} height={40} />{" "}
+          {/* Adjust size as needed */}
         </Link>
       </div>
-      <div className='flex items-center text-white'>
+
+      {/* Navigation Links and User Button */}
+      <div className="flex items-center">
         {!userId && (
           <>
-            <Link
-              href='sign-in'
-              className='text-gray-300 hover:text-white mr-4'
-            >
-              Sign In
+            <Link href="sign-in" className="text-white hover:opacity-75 mr-6">
+              Login
             </Link>
             <Link
-              href='sign-up'
-              className='text-gray-300 hover:text-white mr-4'
+              href="sign-up"
+              className="bg-orange text-white py-2 px-4 rounded hover:bg-orange-700"
             >
-              Sign Up
+              Sign up
             </Link>
           </>
         )}
         {userId && (
-          <Link href='profile' className='text-gray-300 hover:text-white mr-4'>
+          <Link href="profile" className="text-gray-300 hover:text-white mr-4">
             Profile
           </Link>
         )}
-        <div className='ml-auto'>
-          <UserButton afterSignOutUrl='/' />
+        <div className="ml-4">
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </nav>
