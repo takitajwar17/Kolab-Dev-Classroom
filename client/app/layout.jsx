dc1b1328a2c865b73add11c253a76added936ac1
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-10-07 01:13:02 +0600

    added font 'Kanit'

--- client/app/layout.jsx
+++ client/app/layout.jsx
@@ -1,14 +1,19 @@
-import './globals.css';
-import { Inter } from 'next/font/google';
-import { ClerkProvider } from '@clerk/nextjs';
-import { light } from '@clerk/themes';
-import Header from './components/Header';
+import "./globals.css";
+import { Inter, Kanit } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { light } from "@clerk/themes";
+import Header from "./components/Header";
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ["latin"] });
+const kanit = Kanit({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
-  title: 'Sprio Dev Classroom',
-  description: 'Sprio Dev Classroom',
+  title: "Sprio Dev Classroom",
+  description: "Sprio Dev Classroom",
 };
 
 export default function RootLayout({ children }) {
@@ -18,12 +23,12 @@ export default function RootLayout({ children }) {
         baseTheme: light,
       }}
     >
-      <html lang='en'>
-        <body className={inter.className}>
+      <html lang="en">
+        <body className={kanit.className}>
           <Header />
-          <main className='container mx-auto'>
-            <div className='flex items-start justify-center min-h-screen'>
-              <div className='mt-20'>{children}</div>
+          <main className="container mx-auto">
+            <div className="flex items-start justify-center min-h-screen">
+              <div className="mt-20">{children}</div>
             </div>
           </main>
         </body>
