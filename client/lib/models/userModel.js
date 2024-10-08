ec04d7f8b1650fb6ea54faedadb59f0db57281e6
ash_tasnim_1834 <tasnim.iterator.34@gmail.com>
2024-10-09 01:29:22 +0600

    userModel added

--- client/lib/models/userModel.js
+++ client/lib/models/userModel.js
@@ -1,40 +1,37 @@
-import mongoose from 'mongoose';
-
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    clerkId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-    },
-    userName:{
-        type:String,
-        required: true,
-    },
-    firstName: {
-        type: String,
-        required: true,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    }, 
-    image: {
-        type: String, // URL to the image
-        required: false, // Optional field
-    }
-   
-
+  clerkId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  image_url: {
+    type: String, // URL to the image
+    required: false, // Optional field
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
