d21c2a362f7ab9dc2f328c35ba655d3ab2fec5c2
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-10-13 01:56:24 +0600

    after signin -> to dashboard

--- client/app/sign-in/[[...sign-in]]/page.jsx
+++ client/app/sign-in/[[...sign-in]]/page.jsx
@@ -2,16 +2,19 @@
 
 import { useEffect } from "react";
 import { SignIn } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 const SignInPage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const router = useRouter();
+
   return (
     <>
       <div className="flex justify-center w-full mt-6">
-        <SignIn />
+        <SignIn afterSignIn={router.push("/dashboard")} />
       </div>
     </>
   );
