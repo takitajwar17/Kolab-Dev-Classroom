6b48e1c6be0bc165f4722650afde835199b418b7
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2024-10-13 01:41:46 +0600

    clientLayout margin removed

--- client/app/components/ClientLayout.jsx
+++ client/app/components/ClientLayout.jsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 import Sidebar from "./Sidebar";
 
 export default function ClientLayout({ children }) {
   const pathname = usePathname();
-  const isHomePage = pathname === '/';
+  const isHomePage = pathname === "/";
 
   return (
     <>
       {!isHomePage && <Sidebar />}
-      <main className={`w-full ${isHomePage ? '' : 'ml-16'}`}>
+      <main className="w-full">
         <div className="flex items-start justify-center min-h-screen w-full">
-          <div className={`w-full ${isHomePage ? '' : 'mt-20'}`}>{children}</div>
+          <div className="w-full">{children}</div>
         </div>
       </main>
     </>
