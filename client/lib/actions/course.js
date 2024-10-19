a776dca0e3b3fe4d2c990ba29c50476b7cf53adb
ash_tasnim_1834 <tasnim.iterator.34@gmail.com>
2024-10-20 02:35:07 +0600

    action folder for connecting database with frontend

---
+++ client/lib/actions/course.js
@@ -0,0 +1,29 @@
+"use server";
+
+import Course from "../models/courseModel";
+import { connect } from "../mongodb/mongoose";
+
+export const createOrUpdateCourse = async (title, details, courseCode) => {
+  try {
+    await connect();
+    console.log("Creating/updating course: ", { title, details, courseCode });
+
+    const course = await Course.findOneAndUpdate(
+      { courseCode: courseCode },
+      {
+        $set: {
+          title: title,
+          description: details,
+          courseCode: courseCode,
+        },
+      },
+      { new: true, upsert: true }
+    );
+
+    console.log("Course after update:", course);
+    return course.toObject(); // Convert Mongoose document to plain object
+  } catch (error) {
+    console.error("Error creating or updating course:", error);
+    throw error;
+  }
+};
