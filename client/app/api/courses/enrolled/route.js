0e2dc506d8a784851ba37df99c468b7c81d23578
takitajwar17 <tajwaruzzaman@iut-dhaka.edu>
2024-10-25 01:37:26 +0600

    Enrolled Courses Endpoint Implemented

---
+++ client/app/api/courses/enrolled/route.js
@@ -0,0 +1,37 @@
+// route.js
+import Course from "@/lib/models/courseModel";
+import { connect } from "@/lib/mongodb/mongoose";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    await connect();
+    const { userId } = auth();
+
+    if (!userId) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // Fetch courses where the user is enrolled
+    const courses = await Course.find({
+      enrolledStudents: userId,
+    }).populate("tasks", "title description dueDate");
+
+    // If the user is not enrolled in any courses, return a suitable message
+    if (courses.length === 0) {
+      return NextResponse.json({
+        message: "No courses found where user is enrolled.",
+      });
+    }
+
+    // Return the course details including populated task details
+    return NextResponse.json(courses);
+  } catch (error) {
+    console.error("Error fetching enrolled courses:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
