4c0db5de9b06171837058fb259a2b7aa1edb6478
ash_tasnim_1834 <tasnim.iterator.34@gmail.com>
2024-10-25 02:24:46 +0600

    Course Joining api done

--- client/app/courses/page.jsx
+++ client/app/courses/page.jsx
@@ -81,7 +81,7 @@ const CourseSection = () => {
             <div className="absolute right-0 mt-2 w-36 bg-white border-2 border-gray-200 rounded-lg shadow-lg">
               <ul className="text-left">
                 <li className="w-full rounded-lg px-2 py-2 text-center text-gray-700 hover:bg-gray-100 cursor-pointer">
-                  <Link href="/courses/join-class">Join Course</Link>
+                  <Link href="/courses/join-course">Join Course</Link>
                 </li>
                 <li className="w-full rounded-lg px-2 py-2 text-center text-gray-700 hover:bg-gray-100 cursor-pointer">
                   <Link href="/courses/create-course">Create Course</Link>
